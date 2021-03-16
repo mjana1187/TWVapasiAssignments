ed913e9541d4398a2b4b43d50a49dd239b2aa7f6
jana <mjana1187@gmail.com>
2021-03-16 08:00:06 +0530

    Modified

--- ArrayAssignment/Grades.java
+++ ArrayAssignment/Grades.java
@@ -5,16 +5,23 @@ import java.util.Scanner;
 
 public class Grades {
     public static void main(String[] args) {
+        int iMax, iMin;
+        int intGradeSum = iMax = iMin = 0;
         Scanner sc = new Scanner(System.in);
-        int[] iStudGrade = new int[4];
         System.out.println("Enter Student grade: ");
-        for (int i = 0;i<4;i++){
-            iStudGrade[i] = sc.nextInt();
-            int intGradeSum = 0;
-            intGradeSum = intGradeSum + iStudGrade[i];
+        int iNumOfGrades = sc.nextInt();
+        int[] iStudGradeArr = new int[iNumOfGrades];
+
+        for (int i = 0;i<iNumOfGrades;i++){
+            iStudGradeArr[i] = sc.nextInt();
+            intGradeSum = intGradeSum + iStudGradeArr[i];
         }
-        System.out.println("Average : " + Arrays.stream(iStudGrade).average());
-        System.out.println("Highest : " + Arrays.stream(iStudGrade).max());
-        System.out.println("Lowest: " + Arrays.stream(iStudGrade).min());
+        Arrays.sort(iStudGradeArr);
+        System.out.println(Arrays.toString(iStudGradeArr));
+        double dGradeAverage = intGradeSum/iNumOfGrades;
+        System.out.println("Average is "+ dGradeAverage);
+        System.out.println("Highest : " + iStudGradeArr[iNumOfGrades-1]);
+        System.out.println("Lowest: " + iStudGradeArr[0]);
+
     }
 }
